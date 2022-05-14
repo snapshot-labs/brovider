fd82d8e332bf362cad3d390095000f8306a8b63c
Chaitanya <yourchaitu@gmail.com>
2022-05-14 15:40:06 +0530

    Use express-http-proxy (#4)
    * Use express-http-proxy
    
    * Remove body parser
    
    * Remove "s" prefix
    
    * Merge master
    
    Co-authored-by: bonustrack <fabien@bonustrack.co>

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,14 +1,45 @@
 import express from 'express';
+import proxy from 'express-http-proxy';
 import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 
 const router = express.Router();
 
-router.all('/s/:network', async (req, res) => {
+// Set authorization header for the proxy request
+function setAdditionalHeaders(proxyReqOpts, srcReq) {
+  if (srcReq.nodeData.authHeader)
+    proxyReqOpts.headers['Authorization'] = srcReq.nodeData.authHeader;
+  return proxyReqOpts;
+}
+
+function getPathFromURL(url) {
+  const removePrefix = url.replace(/^(http|https):\/\//, '');
+  return removePrefix.indexOf('/') > -1 ? removePrefix.substring(removePrefix.indexOf('/')) : '/';
+}
+
+function setNode(req, res, next) {
   const { network } = req.params;
-  const url = networks[network].rpc[0];
-  console.log(url, req.body);
-  // @TODO pipe / stream relayed request
-  res.json({ url });
-});
+  const node = networks[network].rpc[0];
+  const nodeURL = typeof node === 'object' ? node.url : node;
+  req.nodeData = {
+    url: nodeURL,
+    path: getPathFromURL(nodeURL),
+    network,
+    authHeader:
+      typeof node === 'object' && node.user && node.password
+        ? 'Basic ' + Buffer.from(`${node.user}:${node.password}`).toString('base64')
+        : undefined
+  };
+  next();
+}
+
+router.use(
+  '/:network',
+  setNode,
+  proxy(req => req.nodeData.url, {
+    memoizeHost: false,
+    proxyReqOptDecorator: setAdditionalHeaders,
+    proxyReqPathResolver: req => req.nodeData.path
+  })
+);
 
 export default router;
