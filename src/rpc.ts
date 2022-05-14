abbdc4062b43dcc1b9170d25cfd90a9113011309
bonustrack <fabien@bonustrack.co>
2022-05-14 16:49:31 +0700

    Remove body parser

--- src/rpc.ts
+++ src/rpc.ts
@@ -8,9 +8,8 @@ const router = express.Router();
  * Used to set the Authorization header for the proxy request
  */
 function setAdditionalHeaders(proxyReqOpts, srcReq) {
-  if (srcReq.nodeData.authHeader) {
+  if (srcReq.nodeData.authHeader)
     proxyReqOpts.headers['Authorization'] = srcReq.nodeData.authHeader;
-  }
   return proxyReqOpts;
 }
 
@@ -23,19 +22,18 @@ function setNode(req, res, next) {
   const { network } = req.params;
   const node = networks[network].rpc[0];
   const nodeURL = typeof node === 'object' ? node.url : node;
-  const nodeData = {
+  req.nodeData = {
     url: nodeURL,
     path: getPathFromURL(nodeURL),
     network,
     authHeader:
       typeof node === 'object' && node.user && node.password
-        ? 'Basic ' + Buffer.from(node.user + ':' + node.password).toString('base64')
+        ? 'Basic ' + Buffer.from(`${node.user}:${node.password}`).toString('base64')
         : undefined
   };
-
-  req.nodeData = nodeData;
   next();
 }
+
 router.use(
   '/s/:network',
   setNode,
