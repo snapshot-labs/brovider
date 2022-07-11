1d1d0fac6402a3f9a4723ac627deaf0c412abe44
bonustrack <fabien@bonustrack.co>
2022-07-12 02:37:50 +0700

    Show version

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import rpc from './rpc';
-import { version } from '../package.json';
+import pkg from '../package.json';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +11,10 @@ app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
-app.get('/', (req, res) => res.json({ version, port: PORT }));
+app.get('/', (req, res) => {
+  const commit = process.env.COMMIT_HASH || '';
+  const version = commit ? `${pkg.version}#${commit.substr(0, 7)}` : pkg.version;
+  res.json({ version, port: PORT });
+});
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
