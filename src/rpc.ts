747a490623bf5632af236a1a5c7f151ea5ae3db3
less <fabien@bonustrack.co>
2023-05-30 15:36:04 +0700

    feat: add rpc route

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,109 +1,21 @@
 import express from 'express';
-import proxy from 'express-http-proxy';
-import { JsonRpcProvider } from '@ethersproject/providers';
-import rpcs from './rpcs.json';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const router = express.Router();
-const monitor = Object.fromEntries(
-  Object.keys(rpcs).map(networksId => [
-    networksId,
-    rpcs[networksId].map(rpc => ({
-      rpc
-    }))
-  ])
-);
 
-// Set authorization header for the proxy request
-function setAdditionalHeaders(proxyReqOpts, srcReq) {
-  if (srcReq.nodeData.authHeader)
-    proxyReqOpts.headers['Authorization'] = srcReq.nodeData.authHeader;
-  return proxyReqOpts;
-}
-
-function getPathFromURL(url) {
-  const removePrefix = url.replace(/^(http|https):\/\//, '');
-  return removePrefix.indexOf('/') > -1 ? removePrefix.substring(removePrefix.indexOf('/')) : '/';
-}
-
-function setNode(req, res, next) {
-  const { network } = req.params;
-  const node = rpcs[network] ? rpcs[network][0] : null;
-  if (!node) return res.status(404).send('Network not found');
-  const nodeURL = typeof node === 'object' ? node.url : node;
-  req.nodeData = {
-    url: nodeURL,
-    path: getPathFromURL(nodeURL),
-    network,
-    authHeader:
-      typeof node === 'object' && node.user && node.password
-        ? 'Basic ' + Buffer.from(`${node.user}:${node.password}`).toString('base64')
-        : undefined
-  };
-  next();
-}
-
-router.get('/monitor', async (req, res) => {
-  return res.json(monitor);
-});
-
-async function getBlockNumber(rpc) {
-  try {
-    if(typeof rpc === 'string') rpc = { url: rpc, timeout: 30000 } 
-    const provider = new JsonRpcProvider(rpc);
-    return await provider.getBlockNumber();
-  } catch (e) {
-    return 0;
-  }
-}
-
-async function isFullArchive(rpc) {
-  try {
-    if(typeof rpc === 'string') rpc = { url: rpc, timeout: 30000 } 
-    const provider = new JsonRpcProvider(rpc);
-    await provider.getBalance('0xeF8305E140ac520225DAf050e2f71d5fBcC543e7', 1);
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-router.use(
-  '/:network',
-  setNode,
-  proxy(req => req.nodeData.url, {
-    timeout: 30000,
-    memoizeHost: false,
-    proxyReqOptDecorator: setAdditionalHeaders,
-    proxyReqPathResolver: req => req.nodeData.path
+router.post(
+  '/1',
+  createProxyMiddleware({
+    target: 'https://eth-mainnet.g.alchemy.com/v2/ZWXqGt01pfN6SG6PuyZ2m9o236iv8iCl',
+    secure: true,
+    changeOrigin: true,
+    logLevel: 'debug',
+    followRedirects: true,
+    cookieDomainRewrite: 'localhost',
+    onProxyReq: () => console.log('Req'),
+    onProxyRes: () => console.log('Res'),
+    onError: e => console.log(e)
   })
 );
-
-let checkCount = 0;
-async function check() {
-  checkCount++;
-  for (const [network, rpcList] of Object.entries(rpcs)) {
-    for (let i = 0; i < rpcList.length; i++) {
-      console.log('Check network', network, 'index', i);
-      const rpc = rpcList[i];
-      const [blockNumber, isArchive] = await Promise.all([getBlockNumber(rpc), isFullArchive(rpc)]);
-      const oldSuccessTotal = monitor[network][i].success_total || 0;
-      const successReward = blockNumber > 0 ? 1 : 0;
-      const successTotal = oldSuccessTotal + successReward;
-      const successScore = parseFloat(((successTotal * 100) / checkCount).toFixed(2));
-      const result = {
-        rpc,
-        block_number: blockNumber,
-        is_archive: isArchive,
-        success_total: successTotal,
-        success_score: successScore,
-        ts: Math.round(Date.now() / 1e3)
-      };
-      monitor[network][i] = result;
-    }
-  }
-  return check();
-}
-
-check();
 
 export default router;
