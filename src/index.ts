e1f1ec2bbe80028516cdd11430135fc8f63f5ff9
less <fabien@bonustrack.co>
2023-05-30 15:34:45 +0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import rpc from './rpc';
-import pkg from '../package.json';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,10 +10,5 @@ app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
-app.get('/', (req, res) => {
-  const commit = process.env.COMMIT_HASH || '';
-  const version = commit ? `${pkg.version}#${commit.substr(0, 7)}` : pkg.version;
-  res.json({ version, port: PORT });
-});
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
