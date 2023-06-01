384f6df6a8f881dabde9ce18c1b1af2f43717d73
less <fabien@bonustrack.co>
2023-06-01 14:10:56 +0700

    chore: move up home route

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ maxAge: 86400 }));
-app.use('/', rpc);
 app.get('/', (req, res) => {
   const commit = process.env.COMMIT_HASH ?? undefined;
   res.json({ name, version, commit });
 });
+app.use('/', rpc);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
