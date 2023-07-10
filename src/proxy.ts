b950e2adc87a99dc68da068ac55b3dc329730d5c
Dmytro Tolok <tolok.d@gmail.com>
2023-07-10 20:57:20 +0200

    refactor(proxy): simplify the code

--- src/proxy.ts
+++ src/proxy.ts
@@ -29,30 +29,51 @@ function onProxyReq(proxyReq, req) {
   fixRequestBody(proxyReq, req);
 }
 
-const onProxyRes = responseInterceptor(async (responseBuffer, proxyRes, req: any) => {
-  const duration = Date.now() - req.params._start;
-  const node = req.params._node;
+function handleError(node) {
   const i = networks[`_${node.network}`].nodes.findIndex(n => n.url === node.url);
 
+  console.log('error status', node.url);
+
+  networks[`_${node.network}`].algorithm.arms[i].reward(-25e3);
+
+  const query = 'UPDATE nodes SET requests = requests + 1, errors = errors + 1 WHERE url = ?';
+  // possible memory leak
+  db.query(query, [node.url]);
+}
+
+function updateReward(node, duration) {
+  const indexOfNetwork = networks[`_${node.network}`].nodes.findIndex(n => n.url === node.url);
+  networks[`_${node.network}`].algorithm.arms[indexOfNetwork].reward(-Math.abs(duration));
+
+  const query = 'UPDATE nodes SET requests = requests + 1, duration = duration + ? WHERE url = ?';
+  db.query(query, [duration, node.url]);
+}
+
+const onProxyRes = responseInterceptor(async (responseBuffer, proxyRes, req: any) => {
+  const node = req.params._node;
+  
   if (proxyRes.statusCode !== 200) {
-    console.log('error status', node.url);
-
-    networks[`_${node.network}`].algorithm.arms[i].reward(-25e3);
-
-    const query = 'UPDATE nodes SET requests = requests + 1, errors = errors + 1 WHERE url = ?';
-    db.query(query, [node.url]);
-  } else {
-    networks[`_${node.network}`].algorithm.arms[i].reward(-Math.abs(duration));
-
-    const query = 'UPDATE nodes SET requests = requests + 1, duration = duration + ? WHERE url = ?';
-    db.query(query, [duration, req.params._node.url]);
+    handleError(node)
+    return responseBuffer;
   }
 
-  if (proxyRes.statusCode === 200 && proxyRes.headers?.['content-type'] === 'application/json') {
+  updateReward(node, Date.now() - req.params._start);
+    
+  let response;
+  let responseBody;
+  try {
+    responseBody = responseBuffer.toString('utf8');
+    response = JSON.parse(responseBody);
+  } catch (e) {
+    console.log('error parsing response', e);
+    return responseBuffer;
+  }
+  
+  if (proxyRes.headers?.['content-type'] === 'application/json') {
     const options = req.params._archive ? { EX: 60 * 60 } : { EX: 3 };
-    redis.set(req.params._key, responseBuffer.toString('utf8'), options);
+    redis.set(req.params._key, responseBody, options);
 
-    return responseBuffer.toString('utf8');
+    return responseBody;
   }
 
   return responseBuffer;
@@ -60,15 +81,8 @@ const onProxyRes = responseInterceptor(async (responseBuffer, proxyRes, req: any
 
 function onError(e, req) {
   if (!req) return;
-
   const node = req.params._node;
-  const i = networks[`_${node.network}`].nodes.findIndex(n => n.url === node.url);
-  networks[`_${node.network}`].algorithm.arms[i].reward(-25e3);
-
-  console.log('on error', node.url);
-
-  const query = 'UPDATE nodes SET requests = requests + 1, errors = errors + 1 WHERE url = ?';
-  db.query(query, [node.url]);
+  handleError(node);
 }
 
 const proxyRequest = createProxyMiddleware({
