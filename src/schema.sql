9c0df23235a6ead2545a16240f026330c11f0b32
Dmytro Tolok <tolok.d@gmail.com>
2023-07-10 13:12:20 +0200

    fix(sql): remove requests table

--- src/schema.sql
+++ src/schema.sql
@@ -13,13 +13,3 @@ CREATE TABLE nodes (
   INDEX `archive` (`archive`),
   INDEX `created` (`created`)
 );
-
-CREATE TABLE requests (
-  `network` VARCHAR(24) NOT NULL,
-  `method` VARCHAR(512) NOT NULL,
-  `archive` INT(1) NOT NULL,
-  `cache` INT(1) NOT NULL,
-  `count` INT(24) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`network`, `method`, `archive`, `cache`),
-  INDEX `count` (`count`)
-);
