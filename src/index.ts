021b02f55c09039031a47d38c8900e4600986a26
Dmytro Tolok <tolok.d@gmail.com>
2023-07-24 22:54:27 +0200

    feat(ui): integrate ui in server routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import { initLogger, fallbackLogger } from './sentry';
 import rpc from './rpc';
 import { name, version } from '../package.json';
@@ -13,14 +14,23 @@ const PORT = process.env.PORT || 3000;
 initLogger(app);
 startJob();
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return next();
+  } else {
+    const pathToStatic = path.join(__dirname, '..', 'ui', '.output', 'public');
+    return express.static(pathToStatic)(req, res, next);
+  }
+});
+
 app.use(express.json({ limit: '8mb' }));
 app.use(express.urlencoded({ limit: '8mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   const commit = process.env.COMMIT_HASH ?? undefined;
   res.json({ name, version, commit });
 });
-app.use('/', rpc);
+app.use('/api', rpc);
 
 fallbackLogger(app);
 
