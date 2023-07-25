51dcb8a1a22e2109f16769e667ec200cf8a7e097
Dmytro Tolok <tolok.d@gmail.com>
2023-07-25 22:07:45 +0200

    feat(nuxt): add simple pages

---
+++ ui/pages/networks/[id].vue
@@ -0,0 +1,7 @@
+<script setup>
+const route = useRoute();
+</script>
+
+<template>
+  <div>network #{{ route.params.id }}</div>
+</template>
---
+++ ui/pages/networks/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>create node</div>
+</template>
