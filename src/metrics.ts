96fef85b11f824a09202aaaf2bb8e522e574940f
Wan <495709+wa0x6e@users.noreply.github.com>
2023-09-12 15:28:12 +0400

    Update src/metrics.ts
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>

--- src/metrics.ts
+++ src/metrics.ts
@@ -6,6 +6,6 @@ export default function initMetrics(app: Express) {
   init(app, {
     normalizedPath: [['/[0-9]+', '/#network']],
     whitelistedPath: [/^\/$/, /^\/monitor$/, /^\/[0-9]+$/],
-    errorHandler: (e: any) => capture(e)
+    errorHandler: capture
   });
 }
