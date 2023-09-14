4172671ba8cf04978be85c593145b0cf69a7c791
Wan <495709+wa0x6e@users.noreply.github.com>
2023-09-14 19:47:19 +0900

    chore: upgrade metrics to 1.1.0 (#276)
    * chore: upgrade metrics to support pushgateway
    
    * Update src/metrics.ts
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    ---------
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,29 +1,11 @@
-import init, { client } from '@snapshot-labs/snapshot-metrics';
+import init from '@snapshot-labs/snapshot-metrics';
+import { capture } from '@snapshot-labs/snapshot-sentry';
 import { Express } from 'express';
-
-let server;
 
 export default function initMetrics(app: Express) {
   init(app, {
     normalizedPath: [['/[0-9]+', '/#network']],
-    whitelistedPath: [/^\/$/, /^\/monitor$/, /^\/[0-9]+$/]
-  });
-
-  app.use((req, res, next) => {
-    if (!server) {
-      // @ts-ignore
-      server = req.socket.server;
-    }
-    next();
+    whitelistedPath: [/^\/$/, /^\/monitor$/, /^\/[0-9]+$/],
+    errorHandler: capture
   });
 }
-
-new client.Gauge({
-  name: 'express_open_connections_size',
-  help: 'Number of open connections on the express server',
-  async collect() {
-    if (server) {
-      this.set(server._connections);
-    }
-  }
-});
