6382337bc79d927f36b02e5f6a4680aca176c50a
Dmytro Tolok <tolok.d@gmail.com>
2023-09-21 15:25:24 +0200

    fix(cron): dedup cron job for clusters

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 initLogger(app);
-startJob();
+
+if (process.env.NODE_APP_INSTANCE === '0') {
+  startJob();
+}
 
 app.use(express.json({ limit: '8mb' }));
 app.use(express.urlencoded({ limit: '8mb', extended: false }));
--- src/mysql/init.ts
+++ src/mysql/init.ts
@@ -2,18 +2,17 @@ import mysql from 'mysql2/promise';
 import { ConnectionString } from 'connection-string';
 export { OkPacket, FieldPacket } from 'mysql2/promise';
 
-const config = new ConnectionString(process.env.DATABASE_URL || '');
+const { protocol, hosts, path, ...config } = new ConnectionString(process.env.DATABASE_URL || '');
 const db: mysql.Pool = mysql.createPool({
   ...config,
-  host: config.hosts?.[0].name,
-  port: config.hosts?.[0].port,
+  host: hosts?.[0].name,
+  port: hosts?.[0].port,
   connectionLimit: parseInt(process.env.CONNECTION_LIMIT || '10'),
   multipleStatements: true,
   connectTimeout: 60e3,
-  acquireTimeout: 60e3,
   waitForConnections: true,
   charset: 'utf8mb4',
-  database: config.path?.[0],
+  database: path?.[0],
   queueLimit: 0
 });
 
