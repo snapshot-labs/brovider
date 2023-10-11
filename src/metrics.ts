05968461645709e5491d6a5bc101990762d0dc77
Dmytro Tolok <tolok.d@gmail.com>
2023-10-11 20:07:53 +0200

    fix(ts): add type keyword to import

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,6 +1,6 @@
 import init from '@snapshot-labs/snapshot-metrics';
 import { capture } from '@snapshot-labs/snapshot-sentry';
-import { Express } from 'express';
+import type { Express } from 'express';
 
 export default function initMetrics(app: Express) {
   init(app, {
