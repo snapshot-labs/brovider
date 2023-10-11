27f76ed05cf92adc6476662a8d1fc997593923e1
Dmytro Tolok <tolok.d@gmail.com>
2023-10-11 17:06:10 +0200

    feat(metrics): add metrics to the server

---
+++ src/metrics.ts
@@ -0,0 +1,11 @@
+import init from '@snapshot-labs/snapshot-metrics';
+import { capture } from '@snapshot-labs/snapshot-sentry';
+import { Express } from 'express';
+
+export default function initMetrics(app: Express) {
+  init(app, {
+    normalizedPath: [['/[0-9]+', '/#network']],
+    whitelistedPath: [/^\/$/, /^\/monitor$/, /^\/[0-9]+$/],
+    errorHandler: capture
+  });
+}
