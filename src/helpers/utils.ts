b5c5f9de2b02d727dea46e61024990fd4224b933
Chaitanya <yourchaitu@gmail.com>
2025-07-05 00:05:21 +0530

    Revert "refactor: use middlewares (#513)"
    This reverts commit af14115605a49e478473712aa84c1ca8cf336270.

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,27 @@
+import { nodes } from './nodes';
+
 export async function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function setNode(req, res, next) {
+  const { network } = req.params;
+  const { jsonrpc, id } = req.body || {};
+  const url = nodes[network];
+
+  if (!jsonrpc) {
+    return res.status(400).json({ error: 'Invalid request' });
+  }
+
+  if (!url) {
+    return res.status(404).json({ jsonrpc, id, error: 'Invalid network' });
+  }
+
+  req._node = {
+    url,
+    path: url.substring(url.indexOf('/', url.indexOf('://') + 3) || 0) || '/',
+    network
+  };
+
+  next();
 }
