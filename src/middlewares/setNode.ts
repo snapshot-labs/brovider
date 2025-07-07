f1478a09654a582ba9866b5a07959adfadf9eccb
Wan <495709+wa0x6e@users.noreply.github.com>
2025-07-07 17:14:35 +0400

    fix: patching the middleware refactor (#513) (#515)
    * Reapply "refactor: use middlewares (#513)" (#514)
    
    This reverts commit d3c5a3dae6e8bba0680af4ef38756645da87b2d6.
    
    * fix: fix url parsing to also return the query string

---
+++ src/middlewares/setNode.ts
@@ -0,0 +1,42 @@
+import { capture } from '@snapshot-labs/snapshot-sentry';
+import { NextFunction, Request, Response } from 'express';
+import { nodes } from '../helpers/nodes';
+
+export default function setNode(req: Request, res: Response, next: NextFunction) {
+  const network = req.params[0];
+  const body = req.body || {};
+  const { jsonrpc, id } = body;
+  const url = nodes[network];
+
+  if (!req.body || !jsonrpc) {
+    return res.status(400).json({ error: 'Invalid request' });
+  }
+
+  if (!url) {
+    return res.status(404).json({ jsonrpc, id, error: 'Invalid network' });
+  }
+
+  let path: string;
+  try {
+    const { pathname, search } = new URL(url);
+    path = pathname + search;
+  } catch (err) {
+    capture(err, {
+      contexts: {
+        input: {
+          network,
+          url
+        }
+      }
+    });
+    return res.status(500).json({ jsonrpc, id, error: 'Invalid node URL configuration' });
+  }
+
+  (req as any)._node = {
+    url,
+    path,
+    network
+  };
+
+  next();
+}
